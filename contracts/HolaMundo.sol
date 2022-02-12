416739e1733b8022aa7075ebc70aa3bee7731d01
blvckcenturion <mr.plus.co@gmail.com>
2022-02-12 15:07:15 -0400

    First commit

---
+++ contracts/HolaMundo.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+// La version del compilador debe ser mayor o igual a 0.8.10 y menor a 0.9.0
+pragma solidity ^0.8.10;
+
+contract HolaMundo {
+    string public greet = "Hola mundo!";
+}
